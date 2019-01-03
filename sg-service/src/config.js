c6b4bf9b3af29d8e1d02b1e9ebf4f401f441ee1d
meta0123 <meta0123@gmail.com>
2019-01-03 14:53:57 +0800

    update

--- sg-service/src/config.js
+++ sg-service/src/config.js
@@ -1,36 +1,41 @@
-
 module.exports = {
     debug: true,
 
     initExecute: [
-        'C:/Users/User/Documents/Github/javaserver44/SG1_c/sf_301.php',
-        'C:/Users/User/Documents/Github/javaserver44/SG1_c/sf_303.php',
-        'C:/Users/User/Documents/Github/javaserver44/SG1_c/sf_402.php',
-        'C:/Users/User/Documents/Github/javaserver44/SG1_c/sf_890.php',
+        'C:/Work/javaserver44/SG1_c/sf_301.php',
+        'C:/Work/javaserver44/SG1_c/sf_303.php',
+        'C:/Work/javaserver44/SG1_c/sf_402.php',
+        'C:/Work/javaserver44/SG1_c/sf_890.php',
     ],
     php: "C:/Program Files (x86)/EasyPHP-DevServer-14.1VC11/binaries/php/php_runningversion/php.exe",
     phpExecute: [
         {
             name: "SerGame",
-            path: 'C:/Users/User/Documents/Github/javaserver44/SG1_c/service/serGame.php',
+            path: 'C:/Work/javaserver44/SG1_c/service/serGame.php',
             arguments: [],
             delay: 2000,
             encoding: 'big5',
         }, {
             name: "SerPlay",
-            path: 'C:/Users/User/Documents/Github/javaserver44/SG1_c/service/serPlay.php',
+            path: 'C:/Work/javaserver44/SG1_c/service/serPlay.php',
             arguments: [],
             delay: 2000,
             encoding: 'big5',
         }, {
             name: "SerStarBet2",
-            path: 'C:/Users/User/Documents/Github/javaserver44/SG1_c/service/serStarBet2.php',
+            path: 'C:/Work/javaserver44/SG1_c/service/serStarBet2.php',
             arguments: [],
             delay: 2000,
             encoding: 'big5',
+        },  {
+            name: "SG_RT_BetList",
+            path: 'C:/Work/javaserver44/SG1_c/BListFunc.php',
+            arguments: [],
+            delay: 1500,
+            encoding: 'big5',
         }
+        
     ],
-
 
     logger: {
 
