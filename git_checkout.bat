c695c5365958b9a4bb1f4e3fe5289ba1179bcee7
meta0123 <meta0123@gmail.com>
2019-02-25 13:50:17 +0800

    update

---
+++ git_checkout.bat
@@ -0,0 +1,33 @@
+@echo off
+
+echo 'start git clone'
+
+pause
+
+cd C:\Users\User\Documents\Github\javaserver44
+
+git checkout Casino_4in1
+
+
+cd C:\Users\User\Documents\Github\sg44_a
+
+git checkout Casino_4in1
+
+
+cd C:\Users\User\Documents\Github\sg44_c
+
+git checkout Casino_4in1
+
+
+cd C:\Users\User\Documents\Github\sg44_m
+
+git checkout Casino_4in1
+
+
+cd C:\Users\User\Documents\Github\sg44_w
+
+git checkout Casino_4in1
+
+echo 'git checkout finish'
+
+pause
